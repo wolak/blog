5ca6caaa2f5c5dc587fd782d0c609a205472c6fd
wolakpaul <wolakpaul@gmail.com>
2015-02-07 11:04:07 -0500

    Adds some a skeleton for the routes.

---
+++ application/classes/Controller/blog.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+class Controller_Blog extends Controller_Template {
+
+	public function action_login()
+	{
+	}
+
+	public function action_readers()
+	{
+	}
+
+	public function action_authors()
+	{
+	}
+
+	public function action_posts()
+	{
+	}
+
+	public function action_new()
+	{
+	}
+}
