cee9e1e6810cf1aeaca120b9fa6146c7131cc487
wolakpaul <wolakpaul@gmail.com>
2015-02-07 14:54:24 -0500

    adds and ID to the submit button on the login form

--- application/views/login.php
+++ application/views/login.php
@@ -1,6 +1,6 @@
 <form class="form-signin">
   <h2 class="form-signin-heading">Please sign in</h2>
   <label for="username" class="sr-only">User Name</label>
-  <input type="text" id="username" class="form-control" placeholder="User Name" required autofocus>
-  <button class="btn btn-lg btn-primary btn-block">Sign in</button>
+  <input type="text" id="username" class="form-control" placeholder="User Name" autofocus>
+  <button id="loginSubmit" class="btn btn-lg btn-primary btn-block">Sign in</button>
 </form>
