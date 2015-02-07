aa783f842b2203ec265e0a229275be0bfb5e2cd6
wolakpaul <wolakpaul@gmail.com>
2015-02-07 16:52:31 -0500

    Displays a list of all posts, categorized by date

--- application/views/posts.php
+++ application/views/posts.php
@@ -1,11 +1,19 @@
 <div class="row-fluid">
-  Latest Post:
 <?php
+  //Displays a list of all posts, categorized by date
+  $current_date = NULL;
   foreach ($posts as $post)
   {
+    if ($current_date != date('d-m-Y', strtotime($post->created_on)))
+    {
+      $current_date = date('d-m-Y', strtotime($post->created_on));
+      echo $current_date."<br><hr>";
+
+    }
     echo '<div class="span12">';
     echo '<h3>'.$post->title.'<h3>';
     echo '<p>'.$post->post.'</p>';
+    echo '<span><button data-postID="'.$post->id.'" class="btn">View comments</button></span></div>';
   }
 ?>
 </div>
